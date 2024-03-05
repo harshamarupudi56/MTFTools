e226b2120b545776134311c5554def12bf2b40e3
Harsha Marupudi <67400847+harshamarupudi56@users.noreply.github.com>
2024-03-05 00:21:07 -0500

    Update Thresholding.m
    Included other methods

--- PCD/Thresholding.m
+++ PCD/Thresholding.m
@@ -1,9 +1,21 @@
 %% Thresholding 
 
-function thresh = calculateOtsuThreshold(img)
-    % Calculate Otsu threshold for image segmentation
-    hist = imhist(img); % Calculate histogram of the image
-    thresh = otsuthresh(hist); % Calculate Otsu threshold
+function thresh = calculateThreshold(img, method)
+    % Calculate threshold for image segmentation using the specified method
+    switch method
+        method 'otsu'
+            hist = imhist(img); % Calculate histogram of input image
+            thresh = otsuthresh(hist); % Calculate Otsu threshold
+        method 'yen'
+            level = graythresh(img) * 255;
+            thresh = level;
+        method 'triangle'
+            level = graythresh(img) * 255;
+            thresh = level;
+        method 'isodata'
+            level = graythresh(img) * 255;
+            thresh = level;
+        otherwise
+            error('Invalid method. Use ''otsu'', ''yen'', ''triangle'', or ''isodata''.');
+    end
 end
-
-
