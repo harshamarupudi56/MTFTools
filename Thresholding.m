27b1b66fac625e3c65c957d809a431e8ec942075
Harsha Marupudi <67400847+harshamarupudi56@users.noreply.github.com>
2024-03-12 23:17:09 -0400

    Create Thresholding.m

---
+++ Thresholding.m
@@ -0,0 +1,22 @@
+%% Thresholding 
+
+function thresh = calculateThreshold(img, method)
+    % Calculate threshold for image segmentation using the specified method
+    switch method
+        method 'otsu'
+            hist = imhist(img); % Calculate histogram of input image
+            thresh = otsuthresh(hist); % Calculate Otsu threshold
+        method 'yen'
+            level = graythresh(img) * 255;
+            thresh = level;
+        method 'triangle'
+            level = graythresh(img) * 255;
+            thresh = level;
+        method 'isodata'
+            level = graythresh(img) * 255;
+            thresh = level;
+        otherwise
+            error('Invalid method. Use ''otsu'', ''yen'', ''triangle'', or ''isodata''.');
+    end
+end
+ 
